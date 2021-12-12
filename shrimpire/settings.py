90420dc9e47273795a9d896bf38eefd5bb1f4fce
Olivier Audet-Yang <oaudetyang@gmail.com>
2021-12-11 20:34:43 -0500

    Logging

--- shrimpire/settings.py
+++ shrimpire/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import pathlib
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -116,7 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = str((pathlib.Path(__file__).parent.parent / 'static').resolve())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -149,3 +148,6 @@ MEDIA_URL = "/media/"
 
 
 ASSET_ROOT = (pathlib.Path(__file__).parent.parent / 'website' / 'assets').resolve()
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())

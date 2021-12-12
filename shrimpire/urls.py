5481c7e429c3b9fc2babc3bbbfee971f67d9dc12
Olivier Audet-Yang <oaudetyang@gmail.com>
2021-12-11 22:21:15 -0500

    whitenoise

--- shrimpire/urls.py
+++ shrimpire/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin-ui/', admin.site.urls),
     path('', include('website.urls')),
     path('email/', include(email_urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
